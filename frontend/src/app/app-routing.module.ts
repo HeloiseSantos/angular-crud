be2b86d6289a50b54e620201557f6b63c935a0b9
Heloise Silva Santos <santoshs@taginfraestrutura.com.br>
2021-03-31 20:36:23 -0300

    Cria navegação entre componentes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+
+const routes: Routes = [
+  {
+    path: "", /* Vazio - Path raiz da aplicação */
+    component: HomeComponent
+  },
+  {
+    path: "products", /* Path de component */
+    component: ProductCrudComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
