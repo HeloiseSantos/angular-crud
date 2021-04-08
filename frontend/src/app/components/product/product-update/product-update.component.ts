3fa76187437221fbfd0db46497b55937c08c86e4
Heloise Silva Santos <santoshs@taginfraestrutura.com.br>
2021-04-07 21:02:31 -0300

    Configura atualização de produtos por id

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-update',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+  product: Product;
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    // Permite que quando eu entrar na tela de atualizar, ela já estará com os dados do produto preenchidos com o id
+    const id = this.route.snapshot.paramMap.get('id');
+    this.productService.readById(id).subscribe(product => {
+      this.product = product;
+    });
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage('Produto atualizado com sucesso!');
+      this.router.navigate(['/products']);
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products']);
   }
 
 }
